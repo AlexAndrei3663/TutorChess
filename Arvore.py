888605ada169dd6d0749631adbf445549ac33c69
DESKTOP-A7FH8P7\Alex Andrei <alexandreipf@hotmail.com>
2020-09-23 11:39:34 -0300

    Estrutura de dados da Arvore de busca binaria

---
+++ Arvore.py
@@ -0,0 +1,187 @@
+class Node:
+    def __init__(self, data=None,  left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+        
+    def __repr__(self):
+        return '{} <- {} -> {}'.format(self.left,
+                                       self.data,
+                                       self.right)
+    
+    def __str__(self):
+        return str(self.data)
+
+        
+class BinarySearchTree:
+    def __init__(self, data=None, node=None):
+        if node:
+            self.root = node
+        if data:
+            self.root = Node(data)
+        else:
+            self.root = None
+        
+    def inOrder(self, node="root"):
+        """ Imprime na tela a arvore em ordem simetrica """
+        
+        # Se não for inserido um no
+        # a raiz da arvore eh inserida como no inicial
+        if node == "root":
+            node = self.root
+            
+        # Se node não for vazia, eh impimido o no em ordem simetrica
+        if node:
+            self.inOrder(node.left)
+            print(node,end=" ")
+            self.inOrder(node.right)
+    
+    def preOrder(self, node="root"):
+        """ Imprime na tela a arvore em pre ordem """
+        
+        # Se não for inserido um no
+        # a raiz da arvore eh inserida como no inicial
+        if node == "root":
+            node = self.root
+        
+        # Se node não for vazia, eh impimido o no em pre ordem
+        if node:
+            print(node,end=" ")
+            self.preOrder(node.left)
+            self.preOrder(node.right)
+     
+    def posOrder(self, node="root"):
+        """ Imprime na tela a arvore em ordem simetrica """
+        
+        # Se não for inserido um no
+        # a raiz da arvore eh inserida como no inicial
+        if node == "root":
+            node = self.root
+            
+        # Se node não for vazia, eh impimido o no em pos ordem
+        if node:
+            self.posOrder(node.left)
+            self.posOrder(node.right)
+            print(node,end=" ")
+    
+    def height(self, node="root"):
+        """ Retorna a altura da arvore """
+        
+        # Se não for inserido um no
+        # a raiz da arvore eh inserida como no inicial
+        if node == "root":
+            node = self.root
+            
+        h_left = 0
+        h_right = 0
+        
+        # Descobre qual o maior ramo
+        if node.left:
+            h_left = self.height(node.left)
+        if node.right:
+            h_right = self.height(node.right)
+            
+        #Retorna o valor do maior ramo mais um pelo atual
+        if h_left < h_right:
+            return h_right + 1
+        return h_left + 1
+        
+    def min(self, node="root"):
+        """ Retorna o no com o valor minimo na arvore """
+        
+        # Se não for inserido um no
+        # a raiz da arvore eh inserida como no inicial
+        if node == "root":
+            node = self.root
+            
+        # Encontra o no mais a esquerda
+        while node.left:
+            node = node.left
+        return node.data
+    
+    def max(self, node="root"):
+        """ Retorna o no com o valor maximo na arvore """
+        
+        # Se não for inserido um no
+        # a raiz da arvore eh inserida como no inicial
+        if node == "root":
+            node = self.root
+            
+        # Encontra o no mais a direita
+        while node.right:
+            node = node.right
+        return node.data
+        
+    def insert(self, data):
+        """ Insersao do dado na arvore """
+        
+        # Finaliza se não houver valor
+        if data is None:
+            return
+        
+        # Encontra o local para alocar o dado
+        current = self.root
+        parent = None
+        while current:
+            parent = current
+            # Vai para a esquerda se for menor e direita se for maior
+            if data < current.data:
+                current = current.left
+            else:
+                current = current.right
+        
+        # Aloca o dado no local encontrado
+        # Se nao houver raiz, insere na raiz
+        if parent is None:
+            self.root = Node(data)
+        # Insere a esquerda se for menor e a direita se for maior
+        elif data < parent.data:
+            parent.left = Node(data)
+        else:
+            parent.right = Node(data)
+            
+    def search(self, data, node="root"):
+        "Busca o no referente ao valor inserido"
+        
+        # Se não for inserido um no
+        # a raiz da arvore eh inserida como no inicial
+        if node == "root":
+            node = self.root
+        # Se nao encontrar o valor retorna None
+        if node is None:
+            return node
+        # Se achar o valor, retorna seu no
+        if node.data == data:
+            return node
+        # Continua procurando
+        # A esquerda se for menor e a direita se for maior
+        if node.data > data:
+            return self.search(data, node.left)
+        return self.search(data, node.right)
+    
+    def remove(self, data, node="root"):
+        "Remove o no com o dado especificado"
+        
+        # Se não for inserido um no
+        # a raiz da arvore eh inserida como no inicial
+        if node == "root":
+            node = self.root
+        
+        if node is None:
+            return node
+        
+        if data < node.data:
+            node.left = self.remove(data, node.left)
+        elif data > node.data:
+            node.right = self.remove(data, node.right)
+        else:
+            if node.left is None:
+                return node.right
+            elif node.right is None:
+                return node.left
+            else:
+                sub = self.min(node.right)
+                node.data = sub
+                node.right = self.remove(sub, node.right)
+        
+        return node
