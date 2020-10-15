dd9b62821a6c83862b3b775116545d92c9748fb7
jackesfonseca <jackesdafonseca@gmail.com>
2020-10-15 11:52:21 -0300

    criacao do metodo main no gui.py

--- src/gui.py
+++ src/gui.py
@@ -1,1 +1,15 @@
 import tkinter as tk
+from chessGame.boardgame import Board
+
+def main(Board):
+    root = tk.Tk()
+    root.title("Chess")
+    #gui.draw_board()
+    #gui.draw_pieces()
+    root.mainloop()
+
+if __name__ == "__main__":
+    game = Board
+    main(game)
+
+
